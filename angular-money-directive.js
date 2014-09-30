32ee9cf7459d8e739a29b2a75ccfc4a29fc1b5c8
Nathaniel Fitzgerald-Hood <nathanielf@gmail.com>
2014-09-30 13:58:48 +1000

    feat(min): do not touch negative numbers in normal input mode

--- angular-money-directive.js
+++ angular-money-directive.js
@@ -3,32 +3,135 @@
  * /src/ng/directive/input.js
  */
 
+/**
+ * wiwo:
+ * - directive needs to allow users to input what looks like a number - 1,000
+ * - strip everything that parseFloat doesn't accept
+ *
+ * parseFloat acceptable input: 0-9 + - .
+ * all other characters stripped out, if nothing remains after stripping, then ng-invalid = true stop further processing
+ *
+ *
+ * input        outcome
+ * asdf         number invalid
+ * 1,000        valid, number = 1000
+ *
+ *
+ *
+ */
 angular.module('fiestah.money', [])
-    .directive('money', function () {
+    .directive('money', ['$filter', function ($filter) {
         'use strict';
 
-        var NUMBER_REGEXP = /^\s*(\-|\+)?(\d+|(\d*(\.\d*)))\s*$/;
+        var NUMBER_REPLACE_REGEXP = /[^\d.+\-]/g;  // Keep numbers, delimiters and signs.
+        //var NUMBER_REGEXP = /^\s*(\-|\+)?(\d+|(\d*(\.\d*)))\s*$/;
+        var NUMBER_REGEXP = /^\s*(\-|\+)?(\(?\d*\.?\d*\)?)?\s*$/; //allow . without numbers following
 
         function link(scope, el, attrs, ngModelCtrl) {
-            var min = parseFloat(attrs.min || 0);
-            var precision = parseFloat(attrs.precision || 2);
+            var min = 0; //wiwo: min can be dynamically set // parseFloat(attrs.min || 0);
+
+            //wiwo: allow precison to be set to 0
+            //var precision = parseFloat(attrs.precision || 2);
+            var precision = parseInt(attrs.money, 10);
+            if (isNaN(precision)){
+                precision = 2;
+            }
+
             var lastValidValue;
+
+
+            //wiwo: global flag to allow original parsers to continue
+            var continueParsing = true;
 
             function round(num) {
                 var d = Math.pow(10, precision);
                 return Math.round(num * d) / d;
             }
 
+            //function formatPrecision(value) {
+            //    return parseFloat(value).toFixed(precision);
+            //}
+            //wiwo: use ng number filter to get , in the formatter
             function formatPrecision(value) {
-                return parseFloat(value).toFixed(precision);
+                return $filter('number')(value, precision);
             }
 
             function formatViewValue(value) {
                 return ngModelCtrl.$isEmpty(value) ? '' : '' + value;
             }
 
+            /**
+             * wiwo - watch changes to min
+             * @param newMin
+             */
+            function setMin(newMin){
+                if (newMin == 'NEGATIVE_INFINITY'){
+                    min = Number.NEGATIVE_INFINITY;
+                } else {
+                    min = parseFloat(newMin) || 0;
+                }
+            }
+
+            if (attrs.min){
+                // 2014-04-07 Coridyn:
+                // Add watcher on the max validator.
+                attrs.$observe('min', function(newMin){
+                    setMin(newMin);
+                    ngModelCtrl.$setViewValue(ngModelCtrl.$viewValue);
+                });
+
+                // Set the default minimum value.
+                setMin(attrs.min);
+            }
+            // wiwo: end of min
+
+
+            function stripNonNumeric(value) {
+                NUMBER_REPLACE_REGEXP.lastIndex = 0;  // Reset lastIndex for IE.
+                if (ngModelCtrl.$isEmpty(value)) {
+                    return value;
+                } else {
+                    //+"" to be super sure it's a String
+                    return (value+"").replace(NUMBER_REPLACE_REGEXP, '');
+                }
+            }
+
+            /**
+             * wiwo: check for any invalid number (after stripping non-numerics input is empty
+             *
+             * @param value
+             * @returns {*}
+             */
+            function cleanAndCheckNumberValidity(value) {
+
+                continueParsing = true;
+                var stripped = stripNonNumeric(value);
+
+                if (ngModelCtrl.$isEmpty(stripped) && !ngModelCtrl.$isEmpty(value)) {
+                    //an invalid numeric input
+                    continueParsing = false;
+                    ngModelCtrl.$setValidity('number', false);
+
+                    value = undefined;
+
+                } else {
+
+                    value = stripped;
+                    ngModelCtrl.$setValidity('number', true);
+                }
+                return value;
+            }
+
+            ngModelCtrl.$parsers.push(cleanAndCheckNumberValidity);
+
 
             ngModelCtrl.$parsers.push(function (value) {
+
+                //wiwo: fast fail back, our validity has already failed - but leave the displayed value alone
+                if (!continueParsing) {
+                    return value;
+                }
+
                 if (angular.isUndefined(value)) {
                     value = '';
                 }
@@ -39,15 +142,16 @@ angular.module('fiestah.money', [])
                 }
 
                 // Allow "-" inputs only when min < 0
-                if (value.indexOf('-') === 0) {
-                    if (min >= 0) {
-                        value = null;
-                        ngModelCtrl.$setViewValue('');
-                        ngModelCtrl.$render();
-                    } else if (value === '-') {
-                        value = '';
-                    }
-                }
+                //wiwo: this is the concern of min-validation, don't do this here
+                //if (value.indexOf('-') === 0) {
+                //    if (min >= 0) {
+                //        value = null;
+                //        ngModelCtrl.$setViewValue('');
+                //        ngModelCtrl.$render();
+                //    } else if (value === '-') {
+                //        value = '';
+                //    }
+                //}
 
                 var empty = ngModelCtrl.$isEmpty(value);
                 if (empty || NUMBER_REGEXP.test(value)) {
@@ -79,6 +183,13 @@ angular.module('fiestah.money', [])
 
             if (attrs.max) {
                 var max = parseFloat(attrs.max);
+
+                //wiwo: observe max for dynamic updates
+                attrs.$observe('max', function(newMax){
+                    max = parseFloat(newMax);
+                    ngModelCtrl.$setViewValue(ngModelCtrl.$viewValue);
+                });
+
                 var maxValidator = function(value) {
                     if (!ngModelCtrl.$isEmpty(value) && value > max) {
                         ngModelCtrl.$setValidity('max', false);
@@ -105,10 +216,21 @@ angular.module('fiestah.money', [])
 
             el.bind('blur', function () {
                 var value = ngModelCtrl.$modelValue;
-                if (value) {
-                    ngModelCtrl.$viewValue = formatPrecision(value);
+
+                // wiwo: 2013-11-01 Coridyn:
+                // Run through all of the formatters.
+                if (value){
+                    for (var i = 0, formatters = ngModelCtrl.$formatters, len = formatters.length; i < len; i++){
+                        value = formatters[i](value);
+                    }
+                    ngModelCtrl.$viewValue = value;
                     ngModelCtrl.$render();
                 }
+
+                //if (value) {
+                //    ngModelCtrl.$viewValue = formatPrecision(value);
+                //    ngModelCtrl.$render();
+                //}
             });
         }
 
@@ -117,4 +239,5 @@ angular.module('fiestah.money', [])
             require: 'ngModel',
             link: link
         };
-    });
+    }]
+);
